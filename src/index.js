52f21793e931a8ea52d9968ce6c9f1742e50069c
Christian Montoya <christian.montoya@consensys.net>
2022-05-18 17:00:39 -0400

    Clean up undesired MetaMask GitHub files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const answers = [
-  'Certainly', 
-  'Without a doubt', 
-  'Absolutely yes', 
+  'Certainly',
+  'Without a doubt',
+  'Absolutely yes',
   'You betcha',
   'Confirmed',
   'I think so',
@@ -14,30 +14,30 @@ const answers = [
   'Unsure, ask again later...',
   'Hard to say, ask again later...',
   'Undetermined, ask again later...',
-  'Unclear, ask again later...'
-]; 
+  'Unclear, ask again later...',
+];
 
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'random':
-      if(requestObject.min !== undefined && requestObject.max !== undefined) { 
+      if (requestObject.min !== undefined && requestObject.max !== undefined) {
         const min = parseInt(requestObject.min);
-        const max = parseInt(requestObject.max);  
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
+        const max = parseInt(requestObject.max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
       }
-      return Math.random(); 
-    case 'mystery': 
-      const mysteryResponse = answers[ Math.floor(Math.random() * answers.length) ]; 
+      return Math.random();
+    case 'mystery':
+      const mysteryResponse =
+        answers[Math.floor(Math.random() * answers.length)];
       return wallet.request({
-        method: 'snap_confirm', 
+        method: 'snap_confirm',
         params: [
           {
-            prompt: `The Mysterious 🦊 Has Spoken`, 
+            prompt: `The Mysterious 🦊 Has Spoken`,
             description: `You asked: ${requestObject.question}`,
-            textAreaContent: 
-              `The mysterious fox says: ${mysteryResponse}`,  
-          }, 
-        ], 
+            textAreaContent: `The mysterious fox says: ${mysteryResponse}`,
+          },
+        ],
       });
     default:
       throw new Error('Method not found.');
