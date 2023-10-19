4b64e222749e82d9fbbf426f22ac46b792849968
Christian Montoya <christian.montoya@consensys.net>
2023-10-18 21:00:47 -0400

    el zorro dice

--- src/index.js
+++ src/index.js
@@ -1,23 +1,99 @@
 import { panel, text, divider } from '@metamask/snaps-ui';
 
+const uiText = {
+  header: { 
+    en: 'The Mysterious 🦊 Has Spoken', 
+    es: 'El 🦊  Misterioso Dice'
+  },
+  youasked: { 
+    en: 'You asked:', 
+    es: 'Usted preguntó:'
+  },
+  says: { 
+    en: 'The fox says:',
+    es: 'El zorro respondió:'
+  }
+}; 
+
 const answers = [
-  'Certainly',
-  'Without a doubt',
-  'Absolutely yes',
-  'You betcha',
-  'Confirmed',
-  'I think so',
-  'It is likely',
-  'Consensus is yes',
-  'Unlikely',
-  'Unconfirmed',
-  'Not looking good',
-  'Doubtful',
-  'Unsure, ask again later...',
-  'Hard to say, ask again later...',
-  'Undetermined, ask again later...',
-  'Unclear, ask again later...',
+  {
+    en: 'Certainly',
+    es: 'Ciertamente'
+  },
+  {
+    en: 'Without a doubt',
+    es: 'Sin duda'
+  },
+  {
+    en: 'Absolutely',
+    es: 'Absolutamente'
+  },
+  {
+    en: 'You betcha',
+    es: 'Tu lo sabes'
+  },
+  {
+    en: 'Confirmed', 
+    es: 'Confirmado'
+  },
+  {
+    en: 'I think so',
+    es: 'Creo que sí'
+  },
+  {
+    en: 'It is likely',
+    es: 'Es probable'
+  },
+  {
+    en: 'Consensus is yes',
+    es: 'Hay acuerdo'
+  },
+  {
+    en: 'Unlikely',
+    es: 'Improbable'
+  },
+  {
+    en: 'Unconfirmed',
+    es: 'Sin confirmación'
+  },
+  {
+    en: 'Not looking good',
+    es: 'No parece bueno'
+  },
+  {
+    en: 'Doubtful',
+    es: 'Dudoso'
+  }, 
+  {
+    en: 'Unsure, ask again later...',
+    es: 'No estoy seguro, pregúntame más tarde...'
+  },
+  {
+    en: 'Hard to say, ask again later...',
+    es: 'Es dificil decir, pregúntame más tarde...'
+  },
+  {
+    en: 'Undetermined, ask again later...',
+    es: 'No hay indicación, pregúntame más tarde...'
+  },
+  {
+    en: 'Unclear, ask again later...',
+    es: 'No es claro, pregúntame más tarde...'
+  }
 ];
+
+const getLocale = async function() { 
+  const locale = await snap.request({ method: 'snap_getLocale' });
+  switch(locale) { 
+    case 'es': 
+    case 'es_419': 
+      return 'es'; 
+      break;
+    default: 
+      return 'en'; 
+      break; 
+  }
+}; 
 
 module.exports.onRpcRequest = async ({ origin, request }) => {
   switch (request.method) {
@@ -29,19 +105,20 @@ module.exports.onRpcRequest = async ({ origin, request }) => {
       }
       return Math.random();
     case 'mystery':
-      const mysteryResponse = answers[Math.floor(Math.random() * answers.length)];
+      const locale = await getLocale();
+      const mysteryResponse = answers[Math.floor(Math.random() * answers.length)][locale];
       const question = request.params.question.trim();
       return snap.request({
         method: 'snap_dialog',
         params: {
           type: 'alert',  
           content: panel([
-            text(`**The Mysterious 🦊 Has Spoken**`),
+            text(`**${uiText.header[locale]}**`),
             divider(), 
-            text('You asked:'), 
+            text(uiText.youasked[locale]), 
             text(`_${question}_`),
             divider(),
-            text('The mysterious fox says:'),
+            text(uiText.says[locale]),
             text(`**${mysteryResponse}**`),
           ]), 
         },
